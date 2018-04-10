5fc01237e60653cc16efeb4f5d0f956b4fc8a30c
Cairol <Cairol75@gmx.de>
2018-04-10 21:28:22 +0200

    trlte-common: Added overlay directory for ResurrectionRemix

--- trlte.mk
+++ trlte.mk
@@ -23,7 +23,8 @@ $(call inherit-product-if-exists, vendor/samsung/trlte-common/trlte-common-vendo
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-rr
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
