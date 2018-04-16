160313aa99411126903faaeb6bc231e101a73057
Cairol <Cairol75@gmx.de>
2018-04-16 22:11:43 +0200

    trlte-common: Bluetooth: Removed HFP client --> Device acts now as a smartphone when connecting via bluetooth

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -39,7 +39,4 @@ static inline const char* BtmGetDefaultName()
 
 #define BTE_BLE_STACK_CONF_FILE "/etc/bluetooth/bt_stack.conf"
 
-/* Handsfree device */
-#define BTA_DM_COD { 0x20, 0x04, 0x08 }
-
 #endif
