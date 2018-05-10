b2ba14fa082dbba5a642ebfbd0883ffc95bcbaad
Cairol <Cairol75@gmx.de>
2018-05-10 21:23:12 +0200

    trlte-common: added inherit from the proprietary version

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -85,3 +85,6 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/etc/wifi/bcmdhd_apsta.bin"
 WIFI_BUS := PCIE
+
+# inherit from the proprietary version
+-include vendor/samsung/trlte-common/BoardConfigVendor.mk
